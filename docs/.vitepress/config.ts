fba3f5286216ac4e555277267149cb3f22f92cdb
蛟龙 <75253383+TheFloodDragon@users.noreply.github.com>
2024-06-02 09:27:44 +0800

    Update Upstream

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -34,10 +34,11 @@ export default defineConfig({
 
     nav,
     sidebar,
+
     /* 右侧大纲配置 */
     outline: {
       level: [1, 6],
-      label: '本页目录',
+      label: '目录',
     },
 
     socialLinks: [{ icon: 'github', link: 'https://github.com/postyizhan/lezi-wiki' }],
@@ -47,15 +48,26 @@ export default defineConfig({
       copyright: 'Copyright © 2019-present maomao',
     },
 
-    darkModeSwitchLabel: '外观',
-    returnToTopLabel: '返回顶部',
-    lastUpdatedText: '上次更新',
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
+    },
 
     docFooter: {
       prev: '上一篇',
       next: '下一篇',
     },
 
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+
+    /*** 自定义配置 ***/
     visitor: {
       badgeId: 'postyizhan.lezi-wiki',
     },
