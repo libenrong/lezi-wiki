f753dc68ccc235f6830477037716d99f3f9fb3ab
Kobe ⑧ <102713261+HaHaWTH@users.noreply.github.com>
2024-07-13 21:31:01 +0800

    Mybad

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -249,8 +249,8 @@ export const NAV_DATA: NavData[] = [
       {
         icon: 'https://tse3-mm.cn.bing.net/th/id/OIP-C.zOBkqmN4CgZ_yagT1_yswwAAAA?rs=1&pid=ImgDetMain',
         title: '闪星传',
-        desc: '大脑发育不健全只会骚扰群友的傻逼',
-        link: '/lezi-wiki/article/闪星传,
+        desc: '鉴定为玩原神玩的',
+        link: '/lezi-wiki/article/闪星传',
       },
     ],
   },
